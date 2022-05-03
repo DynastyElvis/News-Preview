81eec2a521676068db5e6440a18f6a097c36d174
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-03 14:09:01 +0300

    ft: UNTTEST

--- app/test_request.py
+++ app/test_request.py
@@ -13,7 +13,21 @@ def setUp(self):
     Set up method to run before each test cases.
     '''
     self.new_request = Request('news','news','news','news','news') # create user object
-
+    
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        Request.requests = []
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_request.id, 'news')
+        self.assertEqual(self.new_request.name, 'news')
+        self.assertEqual(self.new_request.email, 'news')
+        self.assertEqual(self.new_request.username, 'news')
+        self.assertEqual(self.new_request.password, 'news')
 
 
 
