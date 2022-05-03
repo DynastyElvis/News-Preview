1aa64da276470ab4641fb6959c7da1ba3e57026b
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-03 13:57:42 +0300

    ft: adding the server command

--- manage.py
+++ manage.py
@@ -10,4 +10,5 @@ This includes running a development server, a customised Python shell, ... from 
 
 app = create_app('development')# creating the app instance
 manager = Manager(app) # creating the manager instance
+
 manager.add.command('server',Server) # adding the server command
