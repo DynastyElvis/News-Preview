0601b09996ac6d596b1000f63e2573a082e647d5
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-03 13:54:26 +0300

    ft: manage.py update

---
+++ manage.py
@@ -0,0 +1,9 @@
+from app import create_app
+from flask_script import Manager,Server
+
+# Creating app instance
+
+'''
+This includes running a development server, a customised Python shell, ... from flask_script
+
+'''
