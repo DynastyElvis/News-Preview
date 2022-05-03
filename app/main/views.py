f6019a7856d80a4a4208d5c09ac201af9312fbd9
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-03 10:54:03 +0300

    ft: added tittle and rendering template html doc in the views file

--- app/main/views.py
+++ app/main/views.py
@@ -8,3 +8,15 @@ __name__ is a special variable that is set to the name of the module in which it
 """
 #views
 
+@main.route('/')
+def index():
+    """
+    View root page function that returns the index page and its data
+    """
+    # Getting news
+    news = get_news()
+    title = 'Home - Welcome to the best News Highlighter Website Online'
+    return render_template('index.html', title = title, articles = news)
+
+    """__name__ is a special variable that is set to the name of the module in which it is used.
+    """
