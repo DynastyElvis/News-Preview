7be2a110ba8ad3a88bcec66cbad6ee8dfd5080c5
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-03 10:49:05 +0300

    ft: added requsts import get_news in views.py logic

---
+++ app/main/views.py
@@ -0,0 +1,10 @@
+from flask import render_template 
+from . import main 
+from ..request import get_news 
+
+"""
+__name__ is a special variable that is set to the name of the module in which it is used.
+
+"""
+#views
+
