4174e0923a3f98aad705ea7db1b985570106cfb0
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-03 12:13:12 +0300

    getting the news url and api key update

--- app/request.py
+++ app/request.py
@@ -15,3 +15,18 @@ def configure_request(app): # configuring the request
     
     """_request_url is a function that returns the url for the news summary request
     """
+
+def get_news(): # getting the news
+    get_news_url = base_url.format(api_key) # getting the news url
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+        
+        news_results = None
+        
+        if get_news_response['articles']:
+            news_results_list = get_news_response['articles']
+            news_results = process_results(news_results_list)
+            
+        return news_results
