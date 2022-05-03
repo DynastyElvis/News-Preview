bfb135431ebe80d89f5aba7ff3291aab34733dec
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-03 12:20:27 +0300

    bg: fixed api key bug

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,4 @@
-NEWS_API_.keys()
-
+NEWS_API_Key = 'aa47b48af14d4200bab1105dfc0fe02e'
 '''
 News API is a simple, easy-to-use REST API that returns JSON search results for current and historic news articles published by over 80,000 worldwide sources.
 
