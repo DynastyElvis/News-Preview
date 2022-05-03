fb78bcc25b9fad411c76030d57929575e792954b
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-03 13:57:11 +0300

    ft: manage.py update

--- manage.py
+++ manage.py
@@ -7,3 +7,7 @@ from flask_script import Manager,Server
 This includes running a development server, a customised Python shell, ... from flask_script
 
 '''
+
+app = create_app('development')# creating the app instance
+manager = Manager(app) # creating the manager instance
+manager.add.command('server',Server) # adding the server command
